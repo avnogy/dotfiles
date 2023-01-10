f0f015f73b8d49dcffe43830410077e980521f58
avnogy <avner.mindelis@gmail.com>
2023-01-10 17:36:56 +0200

    removed unsupported languages

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -183,7 +183,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- [[ Configure Treesitter ]]
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c#' ,'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'html' ,'help' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'html' ,'help' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
