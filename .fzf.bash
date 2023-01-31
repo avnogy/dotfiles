3f8190037c17d668f06339a4b0b23b2f1e9e7021
avnogy <avner.mindelis@gmail.com>
2023-01-31 15:45:26 +0200

    added fzf config

---
+++ .fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/linux/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/linux/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/linux/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/linux/.fzf/shell/key-bindings.bash"
