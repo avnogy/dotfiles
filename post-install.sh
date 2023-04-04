e11150bc5371f0753c50a5660d6d60790033354b
avnogy <avner.mindelis@gmail.com>
2023-04-04 12:54:52 +0300

    enabled everything in post install script

---
+++ post-install.sh
@@ -0,0 +1,12 @@
+curl -sLf https://spacevim.org/install.sh | bash
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+zsh
+nvim
+yadm clone -f https://github.com/avnogy/yadm
+yadm reset --hard origin/master
+nvim
+chsh -s $(which zsh)
+git config --global user.email "avner.mindelis@gmail.com"
+git config --global user.name "avnogy"
+
