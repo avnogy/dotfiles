870c4599753f8437d45eaa0ffb48cd647742f643
avnogy <avner.mindelis@gmail.com>
2023-07-12 17:49:41 +0300

    Updated install.sh

--- dot_scripts/install.sh
+++ dot_scripts/install.sh
@@ -1,71 +1,40 @@
 #!/bin/bash
+echo "Installing chezmoi."
+sh -c "$(curl -fsLS git.io/chezmoi)"
 
-# Function to check if a command is available
-function command_exists() {
-  command -v "$1" >/dev/null 2>&1
-}
+echo "Initialize chezmoi with the source repository.."
+~/bin/chezmoi init https://github.com/avnogy/dotfiles
 
-# Function to ask user for installation confirmation
-function confirm_installation() {
-  if ! command_exists "$1"; then
-    read -p "Do you want to install $1? (y/n): " choice
-    case "$choice" in 
-        y|Y )
-            return 0
-            ;;
-        * )
-            return 1
-            ;;
-    esac
-  else
-    echo "detected $1 is already installed."
-    return 1
+echo "Backing up dotfiles..."
+backup_folder="dotfiles_backup"
+mkdir -p "$backup_folder"
+dotfiles=(
+  ".aliasrc"
+  ".bashrc"
+  ".fzf.zsh"
+  ".lscolors"
+  ".profile"
+  ".tmux.conf"
+  ".vimrc"
+  ".wslrc"
+  ".zprofile"
+  ".zshrc"
+)
+
+for file in "${dotfiles[@]}"; do
+  if [ -e "$HOME/$file" ]; then
+    mv "$HOME/$file" "$backup_folder/"
+    echo "Moved $file to $backup_folder/"
   fi
-}
+done
 
-
-# Install zsh
-if confirm_installation "zsh"; then
-  echo "Installing zsh..."
-  #...
-  chsh -s $(which zsh)
-  zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh)
+scripts_folder=".scripts"
+if [ -d "$HOME/$scripts_folder" ]; then
+  mv "$HOME/$scripts_folder" "$backup_folder/"
+  echo "Moved $scripts_folder and its contents to $backup_folder/"
 fi
 
-# Install tmux
-if confirm_installation "tmux"; then
-  echo "Installing tmux..."
-  #...
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
-# Install fzf
-if confirm_installation "fzf"; then
-  echo "Installing fzf..."
-  #...
-fi
-
-# Install zoxide
-if confirm_installation "zoxide"; then
-  echo "Installing zoxide..."
-  #...
-fi
-
-# Install vim
-if confirm_installation "vim"; then
-  echo "Installing vim..."
-  #...
-fi
-
-# Install chezmoi
-if confirm_installation "chezmoi"; then
-  echo "Installing chezmoi..."
-  #...
-fi
-
-# Apply dotfiles with chezmoi
-echo "Applying dotfiles with chezmoi..."
-chezmoi init https://github.com/avnogy/dotfiles
+echo "Applying dotfiles with chezmoi...."
 chezmoi apply
 
 echo "Installation completed successfully!"
