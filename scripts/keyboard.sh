d1e55e1cfcc3869b3746a0ce8c3107ba63de7ec0
avnogy <avner.mindelis@gmail.com>
2023-07-12 17:08:39 +0300

    moved scripts directory

--- scripts/keyboard.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-bindkey "^[[A" history-beginning-search-backward
-bindkey "^[[B" history-beginning-search-forward
-bindkey "^[[C" forward-char
-bindkey "^[[D" backward-char
--- scripts/net.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh 
-sudo ip addr flush dev eth0
-sudo ip addr add 192.168.123.90/24 dev eth0
-sudo ip link set eth0 up
-sudo ip route add default via 192.168.123.254 dev eth0
