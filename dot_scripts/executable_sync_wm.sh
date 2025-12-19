f2dda5a8028eff2d9141c863fefc5fb9bff29c2a
avnogy <avner.mindelis@gmail.com>
2025-12-19 02:10:01 +0200

    add sync_wm.sh

---
+++ dot_scripts/executable_sync_wm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/github
+
+repos=("dwm" "slstatus" "dmenu" "slock")
+
+for repo in "${repos[@]}"; do
+    echo "Pulling updates for $repo..."
+
+    cd "~/code/$repo" || { echo "Directory $repo not found!"; continue; }
+    git pull mine master
