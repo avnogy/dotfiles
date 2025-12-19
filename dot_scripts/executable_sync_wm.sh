91db017e85640d306a7f14413f9afed172fd8ffc
avnogy <avner.mindelis@gmail.com>
2025-12-19 02:24:26 +0200

    replace tilda with

--- dot_scripts/executable_sync_wm.sh
+++ dot_scripts/executable_sync_wm.sh
@@ -7,7 +7,7 @@ repos=("dwm" "slstatus" "slock")
 for repo in "${repos[@]}"; do
     echo "Pulling updates for $repo..."
 
-    cd "~/code/$repo" || { echo "Directory $repo not found!"; continue; }
+    cd "$HOME/code/$repo" || { echo "Directory $repo not found!"; continue; }
     git pull mine master
     sudo make install
     cd -
