7bef030834664436f52fc9fad9125f1fd55c5dcb
avnogy <avner.mindelis@gmail.com>
2025-12-19 02:18:29 +0200

    compile in sync_wm and remove dmenu

--- dot_scripts/executable_sync_wm.sh
+++ dot_scripts/executable_sync_wm.sh
@@ -2,12 +2,13 @@
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/github
 
-repos=("dwm" "slstatus" "dmenu" "slock")
+repos=("dwm" "slstatus" "slock")
 
 for repo in "${repos[@]}"; do
     echo "Pulling updates for $repo..."
 
     cd "~/code/$repo" || { echo "Directory $repo not found!"; continue; }
     git pull mine master
+    sudo make install
     cd -
 done
