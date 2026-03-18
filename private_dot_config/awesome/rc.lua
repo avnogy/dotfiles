b3622352431cab0e394bd9301202a777d881a6ea
avnogy <avner.mindelis@gmail.com>
2026-03-18 15:39:09 +0200

    awm change default editor to nano

--- private_dot_config/awesome/rc.lua
+++ private_dot_config/awesome/rc.lua
@@ -58,7 +58,7 @@ beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
-editor = os.getenv("EDITOR") or "editor"
+editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
