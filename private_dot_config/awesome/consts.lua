080c848b93bf50cbe8d36a83c6885913b3d13a6e
avnogy <avner.mindelis@gmail.com>
2026-03-19 02:28:16 +0200

    awm split out keybindings and consts

---
+++ private_dot_config/awesome/consts.lua
@@ -0,0 +1,12 @@
+local consts = {}
+
+
+consts.terminal = "alacritty"
+consts.editor = os.getenv("EDITOR") or "nano"
+consts.editor_cmd = consts.terminal .. " -e " .. consts.editor
+
+consts.modkey = "Mod4"
+consts.altkey = "Mod1"
+
+
+return consts
