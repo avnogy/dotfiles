05b9b9d87976fa11e9d0e9400a7f889b5ba8fed1
avnogy <avner.mindelis@gmail.com>
2026-04-23 00:56:50 +0300

    add rounded corners and exclude the awewomewm wibar

--- private_dot_config/exact_awesome/ui/bar.lua
+++ private_dot_config/exact_awesome/ui/bar.lua
@@ -90,7 +90,7 @@ awful.screen.connect_for_each_screen(function(s)
 	})
 
 	-- Create the wibox
-	s.mywibox = awful.wibar({ position = "top", screen = s })
+	s.mywibox = awful.wibar({ position = "top", screen = s, type = "dock" })
 
 	-- Add widgets to the wibox
 	s.mywibox:setup({
