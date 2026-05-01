fcd337b97ffe1066e5861791f4bc6dd2959c45ab
avnogy <avner.mindelis@gmail.com>
2026-05-02 01:21:24 +0300

    Revert "add xinit stuff into xinit.lua.tmpl"
    This reverts commit d3b828588371778507412010a87dd78f52fcd4e2.

--- private_dot_config/exact_awesome/rc.lua
+++ private_dot_config/exact_awesome/rc.lua
@@ -8,13 +8,9 @@ local theme = require("ui.theme")
 require("error_handling.handle_startup_error")
 require("error_handling.handle_runtime_error")
 
-require("startx")
-
 beautiful.init(theme.build())
-
 require("ui.layouts")
 require("ui.bar")
-
 local globalkeys = require("bindings.globalkeys")
 root.keys(globalkeys)
 awful.rules.rules = require("rules")
